c6502ad5fef8bb7ae7fd74d85c93884f3468a432
ucpr <hoge.uchihara@gmail.com>
2019-08-01 00:49:06 +0900

    feat: footerを実装

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+function Footer() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+        2019 @u_chi_ha_ra_
+    </Typography>
+  );
+}
+
+export default Footer;
