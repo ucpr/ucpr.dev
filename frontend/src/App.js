d1256f4b277f0f4f6722cc6beea9b81c3ed4b2f7
ucpr <hoge.uchihara@gmail.com>
2019-08-06 23:23:32 +0900

    feat: mainのコンポーネントとそのcssを定義

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
+
+import Home from './components/Home.js';
+import Profile from './components/Home.js';
+import Works from './components/Home.js';
+import Skill from './components/Home.js';
+import Links from './components/Home.js';
+import Footer from './components/Footer.js';
+
+
+const useStyles = makeStyles(theme => ({
+  box: {
+    backgroundColor: "#2F4052",
+    height: "90%",
+    width: "90%",
+    border: "2px solid #2F4052",
+    borderRadius: "240px 15px 185px 15px / 15px 200px 15px 185px",
+    boxShadow: "0px 0px 20px -5px #2f4052",
+ //   margin: "5%",
+ //   padding: "20%",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container justify="center" className={classes.box}>
+        <BrowserRouter>
+          <Route exact path='/' component={Home} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/works' component={Works} />
+          <Route path='/skill' component={Skill} />
+          <Route path='/links' component={Links} />
+        </BrowserRouter>
+      </Grid>
+
+      <Footer></Footer>
     </div>
   );
 }
