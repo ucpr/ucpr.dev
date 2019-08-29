0878810207aaecde28e42912083199e9c29dec42
ucpr <hoge.uchihara@gmail.com>
2019-08-30 00:52:01 +0900

    feat: デザインを変更

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 
 function Footer() {
   return (
+  <Grid
+    container
+    direction="column"
+    justify="center"
+    alignItems="center"
+  >  
     <Typography variant="body2" color="textSecondary" align="center">
         2019 @u_chi_ha_ra_
     </Typography>
+  </Grid>
   );
 }
 
