9fb597dcc3c0b1cb04006b6647e9efa2fb8026a7
ucpr <hoge.uchihara@gmail.com>
2019-12-26 01:31:16 +0900

    feat: add page and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Home from './pages/home.js';
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+}));
+
 function App() {
+//  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
