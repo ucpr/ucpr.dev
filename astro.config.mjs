4dd861f9d3c3849bd69e23100e7e41f896587124
ucpr <contact@ucpr.dev>
2024-07-20 00:46:25 +0900

    chore: add cfl pages setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
