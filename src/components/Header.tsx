c5cfb83311c58b4e4e1ad79e7599c586841851ca
ucpr <contact@ucpr.dev>
2024-07-20 01:02:51 +0900

    feat: add components

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import type { Component } from "solid-js";
+
+const Header: Component = () => {
+	return (
+		<header class="p-4 container mx-auto flex justify-between items-center bg-red-900">
+			<a class="text-lg font-bold underline" href="/">
+				# ucpr.dev
+			</a>
+			<nav>
+				<ul class="flex space-x-5">
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/profile">Profile</a>
+					</li>
+					<li>
+						<a href="/articles">Articles</a>
+					</li>
+					<li>
+						<a href="/tags">Tags</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
