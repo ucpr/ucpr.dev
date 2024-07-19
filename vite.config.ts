056ffb020b8cddb23118214f65316210f54c1be8
ucpr <contact@ucpr.dev>
2024-07-20 00:05:56 +0900

    chore: add cfl pages setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import mdx from "@mdx-js/rollup";
-import remarkGfm from "remark-gfm";
 
 export default defineConfig({
-	plugins: [
-		solid(),
-	],
-	build: {
-		target: "esnext",
-	},
+  plugins: [solid()],
+  build: {
+    target: "esnext",
+  },
 });
