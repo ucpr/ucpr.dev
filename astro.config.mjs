e7f91c546e736bac8d5f12960aefdbe57116d73d
ucpr <contact@ucpr.dev>
2024-07-21 14:48:22 +0900

    chore: wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,33 @@ import tailwind from "@astrojs/tailwind";
 
 import remarkToc from "remark-toc";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import {
+  transformerNotationDiff,
+  transformerNotationFocus,
+  transformerMetaHighlight,
+} from "@shikijs/transformers";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solidJs(), tailwind()],
-	output: "server",
-	markdown: {
-		remarkPlugins: [[remarkToc, { heading: "contents" }]],
-		rehypePlugins: [rehypeHeadingIds],
-		shikiConfig: {
-			// https://github.com/shikijs/shiki/blob/main/docs/themes.md
-			theme: "one-dark-pro",
-			// https://github.com/shikijs/shiki/blob/main/docs/languages.md
-			langs: [],
-			wrap: false,
-		},
-	},
-	adapter: cloudflare(),
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: "contents" }]],
+    rehypePlugins: [rehypeHeadingIds],
+    shikiConfig: {
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "one-dark-pro",
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      wrap: false,
+      transformers: [
+        transformerNotationDiff,
+        transformerNotationFocus,
+        transformerMetaHighlight,
+      ],
+    },
+  },
+  adapter: cloudflare(),
 });
--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,10 +1,19 @@
-import { getCollection, getEntry } from "astro:content";
+import { getCollection } from "astro:content";
 
 const ArticleList = async () => {
 	const allArticles = await getCollection("articles");
-	console.log(allArticles);
 
-	return <></>;
+	return (
+		<>
+			<ul>
+				{allArticles.map((article) => (
+					<li key={article.slug}>
+						<a href={`/articles/${article.slug}`}>{article.title}</a>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default ArticleList;
